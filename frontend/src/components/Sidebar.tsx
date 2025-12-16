4858df05e83abba87e4d864e3a0d27ca11ab8dbe
jeevanvishnu <jeevanvishnu18@gmail.com>
2025-12-16 13:40:45 +0530

    Add signup page

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -7,6 +7,7 @@ import { PiSignInBold } from "react-icons/pi";
 import { MdEditDocument } from "react-icons/md";
 import Logout from './Logout';
 const Sidebar = () => {
+  const authUser = false;
   return (
     <aside className='flex flex-col items-center min-w-12 sm:w-16 sticky top-0 left-0 h-screen py-8 overflow-y-auto border-r bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 
 		hover:bg-gray-600/10 border-gray-800 text-white' >
